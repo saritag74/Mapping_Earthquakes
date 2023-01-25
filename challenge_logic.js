af28f0278f298b8df7dc0e0e2d19c74692b44eb0
saritag74 <115046550+saritag74@users.noreply.github.com>
2023-01-25 02:54:02 -0500

    Add files via upload

--- challenge_logic.js
+++ challenge_logic.js
@@ -143,7 +143,7 @@ legend.onAdd = function() {
 
 
   // 3. Use d3.json to make a call to get our Tectonic Plate geoJSON data.
-  d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(() => {
+  d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(() =>{
     L.geoJson(plateData, {
       color: "#ff6500",
       weight: 2
