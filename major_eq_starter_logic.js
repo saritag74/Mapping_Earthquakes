8e70e54fc70c24dc28540af9ff2eefefb0e4437f
saritag74 <115046550+saritag74@users.noreply.github.com>
2023-01-26 02:41:28 -0500

    Add files via upload

--- major_eq_starter_logic.js
+++ major_eq_starter_logic.js
@@ -126,7 +126,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     stroke: true,
     weight: 0.5
   };
-
+ }
 // 5. Change the color function to use three colors for the major earthquakes based on the magnitude of the earthquake.
 function getColor(magnitudes) {
   if (magnitudes < 5){
@@ -172,7 +172,7 @@ L.geoJson(data, {
  majorEarthquakes.addTo(map);
 
 // 9. Close the braces and parentheses for the major earthquake data.
- });
+ 
 
 // Here we create a legend control object.
 let legend = L.control({
@@ -201,7 +201,7 @@ legend.onAdd = function() {
       "<i style='background: " + colors[i] + "'></i> " +
       magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
     }
-    //return div;
+    return div;
   };
 
   // Finally, we our legend to the map.
@@ -209,9 +209,18 @@ legend.onAdd = function() {
 
 
   // Use d3.json to make a call to get our Tectonic Plate geoJSON data.
-  d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then(() =>{
+  d3.json("https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json").then((plateData)  =>{
+    L.geoJson(plateData, {
+      color: "#ff6500",
+      weight: 2
+    })
+    .addTo(tectonicPlates);
+
+    // Then add the tectonicplates layer to the map.
+    tectonicPlates.addTo(map);
     
     
   });
 
+});
 });
